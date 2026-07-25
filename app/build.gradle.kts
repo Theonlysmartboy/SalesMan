7f3499ae6f8670585c11743b0c3842b818dfb5a6
Theonlysmartboy <o2jose43@gmail.com>
2026-07-25 09:13:41 +0300

    Updated app version name and Android Gradle Plugin
    - **App Versioning**: Incremented `versionName` to 1.30 in `app/build.gradle.kts`.
    - **Build Tools**: Updated Android Gradle Plugin (AGP) from 9.3.0 to 9.3.1 in `libs.versions.toml`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.js.salesman"
         minSdk = 26
         targetSdk = 37
-        versionCode = 2
+        versionCode = 3
         versionName = "1.30"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
