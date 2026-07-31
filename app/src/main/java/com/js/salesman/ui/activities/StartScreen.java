3ac63447c9b6ba228b02ebdc423ffae06741d146
Theonlysmartboy <o2jose43@gmail.com>
2026-07-31 12:20:09 +0300

    simplified the routing logic in StartScreen.java to always delegate authenticated launches to AuthGateActivity. This ensures that the authentication flow (PIN or Biometric) is handled consistently and that the MainActivity is correctly restarted upon success.

--- app/src/main/java/com/js/salesman/ui/activities/StartScreen.java
+++ app/src/main/java/com/js/salesman/ui/activities/StartScreen.java
@@ -13,15 +13,12 @@ import com.js.salesman.utils.NetworkUtil;
 import com.js.salesman.utils.managers.GPSManager;
 import com.js.salesman.utils.managers.SessionManager;
 import com.js.salesman.ui.activities.auth.AuthGateActivity;
-import com.js.salesman.ui.activities.auth.LockActivity;
 import com.js.salesman.ui.activities.auth.LoginActivity;
 import com.js.salesman.utils.managers.PrefsManager;
-import com.js.salesman.utils.managers.SettingsManager;
 
 public class StartScreen extends AppCompatActivity {
     private static final int SPLASH_DELAY = 2500; // 2.5 seconds
     private PrefsManager prefManager;
-    private SettingsManager settingsManager;
     Intent intent;
     private Handler splashHandler;
 
@@ -30,7 +27,6 @@ public class StartScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startscreen);
         prefManager = new PrefsManager(this);
-        settingsManager = new SettingsManager(this);
         splashHandler = new Handler(Looper.getMainLooper());
         splashHandler.postDelayed(() -> {
             if (prefManager.isFirstLaunch()) {
@@ -38,21 +34,9 @@ public class StartScreen extends AppCompatActivity {
                 intent = new Intent(this, OnboardingActivity.class);
             } else {
                 SessionManager session = new SessionManager(this);
-                if (session.isUserIdSet()) { // User is "logged in" by presence of ID, even if token expired for API
-                    if (session.isSessionValid()) {
-                        // Token valid, but might be idle
-                        if (session.isIdleTimeout(settingsManager.getAutoLockTimeMillis())) {
-                            intent = new Intent(this, LockActivity.class);
-                        } else {
-                            // Valid and not idle, but for launch we always want AuthGate per requirements 
-                            // to ensure Biometric/PIN at start
-                            intent = new Intent(this, AuthGateActivity.class);
-                        }
-                    } else {
-                        // Token expired, but user is known. Route to AuthGate (which routes to Login if PIN missing, but usually just Auth)
-                        // Per requirement: "Email/password login should NOT be shown again unless user explicitly logs out"
-                        intent = new Intent(this, AuthGateActivity.class);
-                    }
+                if (session.isUserIdSet()) {
+                    // User is "logged in" by presence of ID. Route to AuthGate to handle security.
+                    intent = new Intent(this, AuthGateActivity.class);
                 } else {
                     // No user ID -> show login
                     intent = new Intent(this, LoginActivity.class);
